d50c7854e0e5b037b2689e74976d5f22fd3b4e75
Lambda_School_Loaner_26 <marguel.gtz@gmail.com>
2021-10-21 10:11:14 -0500

    css styling of the main html

--- index.js
+++ index.js
@@ -1,24 +1,3 @@
 import App from "./lib/app.js";
 
 const app = new App("#app");
-
-app.addComponent({
-  name: "employee",
-  model: {
-    employee: {},
-  },
-  view(model) {
-    console.log("fire view");
-    return singleCardTemplate(model.employee, router.params[1]);
-  },
-  async controller(model) {
-    model.employee = employeeList[router.params[1]];
-
-    const deleteButton = document.querySelector("#deleteButton");
-
-    deleteButton.addEventListener("click", () => {
-      employeeList.splice(router.params[1], 1);
-      location.replace("#/employees");
-    });
-  },
-});
