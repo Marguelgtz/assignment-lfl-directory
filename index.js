fa96703b532663a4361b110368f7e58d6c5ef3b1
Lambda_School_Loaner_26 <marguel.gtz@gmail.com>
2021-10-21 10:14:27 -0500

    stylesheet link and started on cards

--- index.js
+++ index.js
@@ -1,3 +1,33 @@
 import App from "./lib/app.js";
 
 const app = new App("#app");
+
+const cardTemplate = (employee, i) => `
+<a class="employeeCard" href="#/employees/${i}">
+            <div class="cardTitle">${employee.name}</div>
+
+            <div class="cardTop">
+              <p>Phone: ${employee.phoneNum}</p>    <p>Office: ${employee.officeNum}</p>
+            </div>
+          </a>
+  `;
+
+app.addComponent({
+  name: "employees",
+  model: {
+    employees: [],
+  },
+  view(model) {
+    return `
+      <div class="employeesCont">
+        ${model.employees
+          .map((employee, i) => cardTemplate(employee, i))
+          .join("")}
+      </div>
+    `;
+  },
+  async controller(model) {
+    const employees = employeeList;
+    model.employees = employees;
+  },
+});
